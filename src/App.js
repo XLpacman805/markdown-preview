3cb522d4e771e9225ed425b356b5e2bfd0f452fc
Johnny Meza <xlpacman805@gmail.com>
2020-03-12 22:18:50 -0700

    added preview component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import Editor from './Editor.Component';
-import logo from './logo.svg';
+import Preview from './Preview.Component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Editor />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Preview />
       </header>
     </div>
   );
---
+++ src/Preview.Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Preview extends React.Component {
+    render() {
+        return (
+            <div id="preview">
+                <h2>#preview</h2>
+            </div>
+        );
+    } 
+}
+
+export default Preview;
