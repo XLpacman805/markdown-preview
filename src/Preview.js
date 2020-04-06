9f6d5410ff09f46cc53406223071656c59b04380
Johnny Meza <xlpacman805@gmail.com>
2020-04-05 17:44:51 -0700

    Project can now render markdown

--- src/Preview.js
+++ src/Preview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Markdown from 'markdown-to-jsx';
 
 class Preview extends React.Component {
     constructor(props) {
@@ -8,7 +9,7 @@ class Preview extends React.Component {
     render() {
         return (
             <div id="preview">
-                <h2>{this.props.text}</h2>
+                <Markdown>{this.props.text}</Markdown>
             </div>
         );
     } 
